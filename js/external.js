8f128e53a8c1f379cc8ae5d14ec10a702127644e
Lorena Kell <mrs.lorena.kell@gmail.com>
2022-04-13 16:26:40 -0500

    up to question 3

---
+++ js/external.js
@@ -0,0 +1,16 @@
+
+
+
+
+alert("Welcome to my Website!");
+
+let color = prompt("What is your favorite color?");
+console.log(color);
+
+let verifyColor = confirm("You entered: " + color + ". Is this correct?")
+console.log(verifyColor);
+
+let littleMermaid = MermaidAmt;
+let brotherBear = bearAmt;
+let hercules = herculesAmt;
+let price = 3;
